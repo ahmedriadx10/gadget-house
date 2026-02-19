ce7658c5428411c3495925306327143969720319
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-02-19 17:44:30 +0600

    gadget house add to cart logic added in js file

---
+++ scripts/cart.js
@@ -0,0 +1,60 @@
+//Products add to cart
+
+
+const cardParentGet=getElement('products-container')
+
+cardParentGet.addEventListener('click',function(event){
+const clickedButton=event.target
+if(clickedButton.classList.contains('buy-btn')){
+
+const mainProductCard=clickedButton.parentNode.parentNode.parentNode
+const productDetailsCard=clickedButton.parentNode.parentNode
+
+const productImg=mainProductCard.children[0].children[0]
+const productImgSrc=productImg.getAttribute('src')
+
+const productNameHead=productDetailsCard.children[0]
+const productName=productNameHead.innerText
+const productPrice=productNameHead.nextElementSibling.nextElementSibling.children[0].innerText
+const productPriceNumber=numberConverter(productPrice)
+
+const cartDataParent=getElement('user-products-cart-container')
+const cartDataParentSkeletonChild=getElement('skeleton-container')
+const getUserCartListDashboard=getElement('user-order-dashboard')
+const getUserCartListUl=getElement('user-order-list')
+
+
+const userCartDiv=document.createElement('div')
+userCartDiv.innerHTML=`
+    <div class="card card-side   bg-base-100 shadow-sm"> 
+  <figure>
+    <img
+      src="${productImgSrc}" />
+  </figure>
+  <div class="card-body">
+    <div class="flex items-center gap-5"><div class="inline-grid *:[grid-area:1/1]">
+  <div class="status status-warning animate-ping"></div>
+  <div class="status status-warning"></div>
+</div><span class="text-lg font-bold">Your Product 😍</span></div> 
+    <h2 class="font-bold text-2xl">${productName}</h2>
+    <p class="flex justify-between text-lg">price: <span id="products-price-cart">$${productPriceNumber}</span></p>
+
+    <div class="card-actions justify-end">
+      <button class="cart-delete-btn btn btn-primary" >Remove</button>
+    </div>
+  </div>
+</div> 
+
+`
+
+
+console.log(setTotalTk(productPriceNumber))
+cartDataParent.appendChild(userCartDiv)
+cartDataParentSkeletonChild.classList.add('hidden')
+
+}
+
+})
+
+
+
--- scripts/helper.js
+++ scripts/helper.js
@@ -7,3 +7,38 @@ function getElement(putId){
   return foundElement
 
 }
+
+function numberConverter(getValue){
+  return Number(getValue)
+}
+
+function getDashBoardTotal(){
+  const dashBoardPrice=document.getElementById('user-total-price')
+  return Number(dashBoardPrice.innerText)
+}
+
+function setTotalTk(getPrice){
+  const dashBoardPrice=document.getElementById('user-total-price')
+const setTotalTk=Number(dashBoardPrice.innerText)+getPrice
+dashBoardPrice.innerText=setTotalTk
+}
+
+//     <div class="card card-side   bg-base-100 shadow-sm"> 
+//   <figure>
+//     <img
+//       src="./assets/camera.png"
+//       alt="Movie" />
+//   </figure>
+//   <div class="card-body">
+//     <div class="flex items-center gap-5"><div class="inline-grid *:[grid-area:1/1]">
+//   <div class="status status-warning animate-ping"></div>
+//   <div class="status status-warning"></div>
+// </div><span class="text-lg font-bold">Your Product 😍</span></div> 
+//     <h2 class="font-bold text-2xl">Drone </h2>
+//     <p class="flex justify-between text-lg">price: <span id="products-price-cart">0</span></p>
+
+//     <div class="card-actions justify-end">
+//       <button class="cart-delete-btn btn btn-primary" >Remove</button>
+//     </div>
+//   </div>
+// </div> 
