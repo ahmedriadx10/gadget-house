5e2e084a07b0a54ecefd6e1267c822e5c452f484
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-02-20 12:38:37 +0600

    cart icon plus minus count added

--- scripts/cart-plus-minus.js
+++ scripts/cart-plus-minus.js
@@ -9,20 +9,29 @@ getDashBoardUl.addEventListener("click", function (event) {
   const dashBoardTotal = getDashBoardTotal();
   const getLiQuantity =
     getPlusButtonIcon.parentNode.parentNode.children[0].innerText;
-
+  const cartIcon = getElement("cart-count-show");
+  const cartIconDeep = getElement("cart-count-items");
+  const cartIconNumber = numberConverter(cartIcon.innerText);
+  const cartIconDeepNumber = numberConverter(cartIconDeep.innerText);
   if (getPlusButtonIcon.classList.contains("plus-btn")) {
     const getLiPrice =
       getPlusButtonIcon.parentNode.parentNode.parentNode.children[2];
     const calcPlusPrice = numberConverter(getLiPrice.innerText);
     setTotalTk(calcPlusPrice);
-    getPlusButtonIcon.parentNode.nextElementSibling.removeAttribute('disabled')
+    getPlusButtonIcon.parentNode.nextElementSibling.removeAttribute("disabled");
+
+    cartIcon.innerText = cartIconNumber + 1;
+    cartIconDeep.innerText = cartIconDeepNumber + 1;
   }
 
   if (getMinusButtonIcon.classList.contains("minus-btn")) {
     const getLiPrice =
       getMinusButtonIcon.parentNode.parentNode.parentNode.children[2];
     const clackMinusPrice = numberConverter(getLiPrice.innerText);
-
+    if (cartIconDeepNumber > 0) {
+      cartIcon.innerText = cartIconNumber - 1;
+      cartIconDeep.innerText = cartIconDeepNumber - 1;
+    }
     minusMoneyFromDashboradTotal(clackMinusPrice);
   }
 });
